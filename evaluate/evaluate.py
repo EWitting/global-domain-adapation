979cf586c96e4a731aaec3d7f146c60a138f474b
Emiel Witting <e.a.witting@student.tudelft.nl>
2023-05-11 13:28:01 +0200

    Add basic dataset analysis metrics

--- evaluate/evaluate.py
+++ evaluate/evaluate.py
@@ -1,35 +1,51 @@
-"""Contains utility functions that can be used for evaluating a DA model's performance"""
+"""Contains utility functions that can be used for evaluating a dataset and DA model's performance"""
 
 from sklearn.metrics import accuracy_score
+import numpy as np
 
-def evaluate(dataset, model_builder, fit_params):
+
+def analyze_data(dataset) -> dict:
+    """Analyze properties of a dataset using some directly computable statistical metrics, without ML.
+    :param dataset: tuple (xs, ys, xg, yg, xt, yt) where s=source, g=global, t=target.
+    :returns dict with metrics
+    """
+    res = dict()
+    xs, ys, xg, yg, xt, yt = dataset
+    for x, y, name in [
+            (xg, yg, 'global'),
+            (xs, ys, 'source'),
+            (xt, yt, 'target')]:
+        res[f'num-{name}'] = x.shape[0]
+        res[f'uniqueness-{name}'] = np.unique(x, axis=0).shape[0] / x.shape[0]
+        res[f'class-marginal-{name}'] = np.mean(y)
+    return res
+
+
+def evaluate_model(dataset, model_builder, fit_params: dict) -> dict:
     """Evaluate a domain adaptation model on a dataset, with source only, target only, and global.
     Prints results to console.
 
-    Args:
-        dataset (tuple of Xs, ys, Xg, yg, Xt, yt):
-          data and labels for source, global and target sets
-
-        model_builder (() -> BaseAdaptDeep model):
-          function that returns a new model every call
-
-        fit_params (dict):  
-            parameters like epoch, batch size etc for model.fit
+    :param dataset: (tuple of Xs, ys, Xg, yg, Xt, yt) data and labels for source, global and target sets
+    :param model_builder: (() -> BaseAdaptDeep model) function that returns a new model every call
+    :param fit_params: parameters like epoch, batch size etc. for `model.fit`
+    :returns dict with accuracies 'acc-{name}' for name in {source, target, global}
     """
 
-    Xs, ys, Xg, _, Xt, yt = dataset
-
+    res = dict()
+    xs, ys, xg, yg, xt, yt = dataset
     for name, target in [
-            ("source (supervised)", Xs),
-            ("target (domain adaptation)", Xt),
-            ("global (ours)", Xg)]:
+        ("source (supervised)", xs),
+        ("target (domain adaptation)", xt),
+        ("global (ours)", xg)]:
         print(f"Evaluating {name}...")
-        acc = _eval_single_model(model_builder(), Xs, ys, target, Xt, yt, fit_params)
+        acc = _eval_single_model(model_builder(), xs, ys, target, xt, yt, fit_params)
+        res[f'acc-{name}'] = acc
         print(f"\tDone. Accuracy = {acc}")
+    return res
 
 
-def _eval_single_model(model, Xs, ys, Xt_train, Xt_true, yt, fit_params):
-    """Find the accuraccy of a DA trained on a given target set, on a true set."""
-    model.fit(Xs, ys, Xt_train, **fit_params)
-    yt_pred = model.predict(Xt_true)
+def _eval_single_model(model, xs, ys, xt_train, xt_true, yt, fit_params):
+    """Find the accuracy of a DA trained on a given target set, on a true set."""
+    model.fit(xs, ys, xt_train, **fit_params)
+    yt_pred = model.predict(xt_true)
     return accuracy_score(yt, yt_pred > 0.5)
--- runexperiment.py
+++ runexperiment.py
@@ -1,4 +1,4 @@
-from evaluate.evaluate import evaluate
+from evaluate.evaluate import evaluate_model
 
 from adapt.feature_based import DANN
 from tensorflow.keras.optimizers.legacy import Adam
@@ -9,9 +9,10 @@ from datagen.datagen import make_moons_triplet
 def model_builder():
     """Returns a new DANN model every call"""
     return DANN(loss="bce", optimizer=Adam(0.001, beta_1=0.5),
-                                 lambda_=1, metrics=["acc"], random_state=0)
+                lambda_=1, metrics=["acc"], random_state=0)
+
 
 fit_params = dict(epochs=800, batch_size=34, verbose=0)
 dataset = make_moons_triplet(force_balance=True)
 
-evaluate(dataset, model_builder, fit_params)
+evaluate_model(dataset, model_builder, fit_params)
