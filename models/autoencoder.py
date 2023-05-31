037c0ea0acb505e06a52b40c806629543902ee12
Emiel Witting <e.a.witting@student.tudelft.nl>
2023-05-31 10:41:06 +0200

    fix unused autoencoder parameters

--- models/autoencoder.py
+++ models/autoencoder.py
@@ -5,7 +5,7 @@ from tensorflow import keras
 MISSING_LABEL_VALUE = -1
 
 
-def combined_loss(target_weight: float = 1.0):
+def combined_loss(target_weight: float = 1.0, decoder_weight: float = 1.0):
     """Return a new loss function that combines autoencoder and classification loss,
      where missing labels (target data) are ignored for classification.
      :return: loss function for a [decoder output, classifier output] format"""
@@ -30,19 +30,21 @@ def combined_loss(target_weight: float = 1.0):
         autoencoder_loss = tf.math.reduce_mean(autoencoder_loss)
         masked_classifier_loss = tf.math.reduce_mean(masked_classifier_loss) / (1-masked_proportion)
 
-        return autoencoder_loss + masked_classifier_loss
+        return decoder_weight * autoencoder_loss + masked_classifier_loss
 
     return loss
 
 
 class Autoencoder:
-    def __init__(self, input_dim: int, target_weight: float = 1.0, encoder=None,
-                 decoder=None, classifier=None):
+    def __init__(self, input_dim: int, encoder=None, decoder=None, classifier=None,
+                 target_weight: float = 1.0, decoder_weight: float = 1.0):
         self.input_dim = input_dim
-        self.target_weight = target_weight
         self.encoder = encoder
         self.decoder = decoder
         self.classifier = classifier
+        self.target_weight = target_weight
+        self.decoder_weight = decoder_weight
+
         self.classifier_model, self.combined_model = self._build_model()
         self.history_ = None
 
@@ -71,8 +73,7 @@ class Autoencoder:
         classifier_model = keras.Model(input_layer, classifier_output)
         combined_model = keras.Model(inputs=input_layer, outputs=[decoder_output, classifier_output])
         combined_model.compile(optimizer='adam',
-                               loss=combined_loss(),
-                               loss_weights=[1.0, 1.0])  # Adjust the loss weights as needed
+                               loss=combined_loss(self.target_weight, self.decoder_weight))
         return classifier_model, combined_model
 
     def fit(self, xs, ys, xt, **fit_params):
@@ -80,10 +81,7 @@ class Autoencoder:
         y_train = np.concatenate([ys, np.full(len(xt), MISSING_LABEL_VALUE)])
         hist = self.combined_model.fit(x_train, [x_train, y_train], shuffle=True, **fit_params)
         self.history_ = hist.history
-
-        combined_loss(1.0)([x_train, y_train], self.combined_model.predict(x_train))
-
         return self
 
     def predict(self, x, verbose=0):
-        return self.classifier_model.predict(x, verbose)
+        return self.classifier_model.predict(x, verbose=verbose)
